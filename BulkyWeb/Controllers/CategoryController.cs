d79d7cdad4c3aeb8e854d95a62731abc6ab4256c
Junaid <130550913+jdKheppar@users.noreply.github.com>
2024-07-29 12:13:57 +0500

    crud without salutation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -19,5 +19,74 @@ namespace BulkyWeb.Controllers
 
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.JCategories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            // RedirectToAction("ViewName","ControllerName") but since we are in the same controller we can skip that
+        }
+        //Below function acts as a get function/request
+        public IActionResult Edit(int? Id)
+        {
+            if(Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.JCategories.Find(Id);
+            //The below code will use linked operation
+            //FirstOrDefault will work even if the attribute passed like in our case Id
+            // is not the primary
+            //Category? categoryFromDb1 = _db.JCategories.FirstOrDefault(u=>u.Id==Id);
+            //Category categoryFromDb2 = _db.JCategories.Where(u=>u.Id==Id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //Below function acts as a post function/request
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+            //If you click on the above Edit keyword in visual studio, you will get an option to add a new related to this Controller
+        {
+            _db.JCategories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            // RedirectToAction("ViewName","ControllerName") but since we are in the same controller we can skip that
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.JCategories.Find(Id);
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? Id)
+        {
+            Category? obj = _db.JCategories.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,6 +8,7 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
